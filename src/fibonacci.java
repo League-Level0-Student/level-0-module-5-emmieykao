895e45f77001684cab14d20ba5b46efa16f50e34
emmiekao <emmiekao@localhost>
2020-10-09 18:27:51 -0700

    ring

--- src/fibonacci.java
+++ src/fibonacci.java
@@ -1,6 +1,16 @@
 
 public class fibonacci {
 public static void main(String[] args) {
-	
+	int number1=0;
+	int number2=1;
+	System.out.println(number1);
+	System.out.println(number2);
+	for (int i = 0; i < 10; i++) {
+		System.out.println(number1+number2);
+		number1= number2+number1;
+		
+		System.out.println(number1+number2);
+		number2=number1+number2;
+	}
 }
 }
