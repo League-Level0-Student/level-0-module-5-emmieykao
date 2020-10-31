a0a451b8309f93fe2d844af86e9648624a8ccc8a
emmiekao <emmiekao@localhost>
2020-10-30 18:27:25 -0700

    for loop onslaught

--- src/_04_practice/_1_skill_practice/skill_practice.java
+++ src/_04_practice/_1_skill_practice/skill_practice.java
@@ -1,5 +1,7 @@
 package _04_practice._1_skill_practice;
 
+import java.util.Random;
+
 import javax.swing.JOptionPane;
 
 public class skill_practice {
@@ -8,14 +10,14 @@ public class skill_practice {
 		public static void main(String[] args) {
 
 		skill_practice skills = new skill_practice();
-		           skills.skill1();
+		           //skills.skill1();
 		           skills.skill2();
 		           skills.skill3();
 		           skills.skill4();
 		           skills.skill5();
 		}
 
-		void skill1() {
+		/*void skill1() {
 		// Use pop-ups for the following.
 		// Ask the user how many dimes they have
 String x = JOptionPane.showInputDialog(null, "How many dimes do you have?");
@@ -25,25 +27,31 @@ int num = Integer.parseInt(x);
 
 		// Tell them how many cents they have (hint multiply by 10)
 
-JOptionPane.showMessageDialog(null, "You have " + 10*num + "cents.");
+JOptionPane.showMessageDialog(null, "You have " + 10*num + " cents.");
 
 
 		// Ask the user how tall they are (inches)
 
-String in = JOptionPane.showInputDialog("How tall are you?");
+String in = JOptionPane.showInputDialog("How tall are you? (inches, please!)");
 int height = Integer.parseInt(in);
 
 
 		// If they are shorter than 36 inches, tell them to eat their Wheaties
+if (height<36) {
+	JOptionPane.showMessageDialog(null, "Eat yout Wheaties!");
+}
 
 
 
-
-		}
+		}*/
 
 		void skill2() { // Write a loop to print every third number between 1 and 30 to the console 
 
-
+for (int i = 1; i < 30; i++) {
+	if(i%3==0) {
+		System.out.println(i);
+	}
+}
 
 
 
@@ -53,51 +61,60 @@ int height = Integer.parseInt(in);
 
 		void skill3() { // Get a random number that is less than 20 and print it to the console 
 
-
-
+Random x=new Random();
+int number=x.nextInt(20);
 		// Get another random number that is less than 10 and print it to the console 
-
+Random y= new Random();
+int number2=y.nextInt(10);
 
 
 		// Using a pop-up, tell the user the difference between the numbers // Hint: use subtraction 
-
-
+int z=number-number2;
+JOptionPane.showMessageDialog(null, "The difference between your two numbers is " + z + ".");
 
 		}
 
 		void skill4() { // In a pop-up, ask the user for the city they live in 
 
-
+String city =JOptionPane.showInputDialog(null, "What city do you live in?");
 
 		// If they answered "San Diego", tell them they live in America's Finest City 
 
-
+if(city.equals("San Diego")){
+	JOptionPane.showMessageDialog(null, "You live in America's Finest City.");
+}
 
 		// Otherwise, tell them to move to San Diego 
-
+else {
+	JOptionPane.showMessageDialog(null, "You should move to San Diego.");
+}
 
 
 		// Create a variable - cars - and initialize it to the number of cars your family has. // If there are 0 cars, use a pop-up to display, "I bet you use public transportation." 
+int cars=4;
 
+if (cars==0) {
+	JOptionPane.showMessageDialog(null, "You must use public transportation a lot!");
+}
 
 
 		// If there is 1 car, use a pop-up to display the make/model of the car 
-
-
+else if(cars==1) {
+JOptionPane.showMessageDialog(null, "Your car is a BMW.");}
 
 		// If there is more than 1 car, use a pop-up to display how many wheels the // cars have between them. 
-
-
+else {
+JOptionPane.showMessageDialog(null, "Your car(s) have " + 4*cars + " wheels.");}
 
 		}
 
 		void skill5() { // In a pop-up, ask the user for the name of their school 
 
-
+String school=JOptionPane.showInputDialog("What is the name of your school?");
 
 		// In another pop-up, tell the user, that their school is a fantastic school. // You must include the name of the school in the message. 
 
-
+JOptionPane.showMessageDialog(null, school + " is a fantastic school!");
 
 		}
 		}
--- src/_04_practice/_2_code_4_life/Code4Life.java
+++ src/_04_practice/_2_code_4_life/Code4Life.java
@@ -15,19 +15,29 @@ public class Code4Life {
 	 * *****************/
 	static boolean canPlaySounds = true;
 	
+	public static void main(String[] args) {
+		
 	
 	// 1. Ask the user how many hours they spent coding this week.
-
+String code=JOptionPane.showInputDialog("How many hours did you spend coding this week?");
+int hrs=Integer.parseInt(code);
 	// 2. If it is less than or equal to 2, tell them to stop watching YouTube and
 	// write code instead.
-
+if(hrs<=2) {
+	JOptionPane.showMessageDialog(null, "Stop watching YouTube and code!");
+}
 	// 3. If it is greater than or equal to 3 AND less than or equal to 5, tell them
 	// they're a Code Ninja
-
+else if (hrs>=3&&hrs<=5) {
+	JOptionPane.showMessageDialog(null, "You are a Code Ninja!");
+}
 	// 4. If it is more than 5, call the playBatmanTheme  method, then 
 	//    call the displayBatman method.
-
-
+else if(hrs>5) {
+	playBatmanTheme();
+	displayBatman();
+}
+	}
 
 	
 	private static void playBatmanTheme() {
